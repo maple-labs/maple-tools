de04990e5a372564ca0c5481e8fd79716c0ff54a
Lucas <lucas@maple.finance>
2021-07-06 22:54:21 -0400

    feat: pass in params, script works

--- src/calculate-airdrop.js
+++ src/calculate-airdrop.js
@@ -1,85 +1,86 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const path = require('path');
-const { providers, Contract, BigNumber } = require('ethers')
-const csv = require('csvtojson')
+const { providers, Contract, BigNumber } = require('ethers');
+const ObjectsToCsv = require('objects-to-csv');
+const csvToJson = require('csvtojson');
 
+const provider = new providers.JsonRpcProvider(process.env.ETH_RPC_URL);
 
-async function main() {
+const rewardsAbi = [
+    'event RewardPaid(address indexed account, uint256 reward)',
+    'event Staked(address indexed account, uint256 amount)',
+    'function earned(address account) external view returns (uint256)',
+    'function balanceOf(address account) external view returns (uint256)',
+];
+const mplAbi = ['event Transfer(address indexed from, address indexed to, uint256 value)'];
 
-    const provider = new providers.JsonRpcProvider(process.env.ETH_RPC_URL)
+const ORTHOGONAL_REWARDS = '0x7869D7a3B074b5fa484dc04798E254c9C06A5e90';
+const MPL = '0x33349B282065b0284d756F0577FB39c158F935e6';
+const GOVERNOR = '0xd6d4Bcde6c816F17889f1Dd3000aF0261B03a196';
 
-    const inputData = await csv().fromFile(path.resolve(__dirname, 'input.csv'));
+const mpl = new Contract(MPL, mplAbi, provider);
 
-    const MPL = '0x33349B282065b0284d756F0577FB39c158F935e6';
-    const GOVERNOR = '0xd6d4Bcde6c816F17889f1Dd3000aF0261B03a196';
-    const ORTHOGONAL_REWARDS = '0x7869D7a3B074b5fa484dc04798E254c9C06A5e90';
-    const REWARD_ADDED_TIME_1 = 1621398655;
+// Derived from: 2 MPL / 100 USD / 180 days == x MPL / totalStaked / dTime
+// Note: Units work out since totalStaked is denominated in WAD
+const getTheoreticalRewards = (totalStaked, dTime) => {
+    return BigNumber.from('2')
+        .mul(totalStaked)
+        .mul(BigNumber.from(dTime.toString()))
+        .div(BigNumber.from('100'))
+        .div(BigNumber.from((180 * 86400).toString()));
+};
 
-    const rewardsAbi = [
-        'event RewardPaid(address indexed account, uint256 reward)',
-        'function earned(address account) external view returns (uint256)',
-        'function balanceOf(address account) external view returns (uint256)'
-    ];
+const calcAirdrop = async (inputFilename, outputFilename, rewardsAddress, airdropTransferAddress, currentTimestamp) => {
+    const inputData = await csvToJson().fromFile(path.resolve(__dirname, 'csv', inputFilename));
 
-    const mplAbi = [ 'event Transfer(address indexed from, address indexed to, uint256 value)' ];
+    const rewardsContract = new Contract(rewardsAddress, rewardsAbi, provider);
 
-    const orthogonalRewards = new Contract(ORTHOGONAL_REWARDS, rewardsAbi, provider);
-    const mpl = new Contract(MPL, mplAbi, provider);
+    let outputData = await Promise.all(
+        inputData.map(async (user) => {
+            // Get all reward claims from the user for the given pool
+            const rewardPaidFilter = rewardsContract.filters.RewardPaid(user.address);
+            const rewardPaidEvents = await rewardsContract.queryFilter(rewardPaidFilter);
+            const totalClaimed = rewardPaidEvents.reduce((total, obj, i, []) => {
+                return total.add(obj.args.reward);
+            }, BigNumber.from('0'));
 
-    const { timestamp: blockTimestamp } = await provider.getBlock();
+            // Get all MPL transferred from the governor address to account for past airdrops
+            const transferFilter = mpl.filters.Transfer(airdropTransferAddress, user.address);
+            const transferEvents = await mpl.queryFilter(transferFilter);
+            const totalAirdropped = transferEvents.reduce((total, obj, i, []) => {
+                return total.add(obj.args.value);
+            }, BigNumber.from('0'));
 
-    let outputData = await Promise.all(inputData.map(async user => {
+            // Get unclaimed MPL rewards for the user for the given pool
+            const earnings = (await rewardsContract.functions.earned(user.address))[0];
 
-        // Get all reward claims from the user for the given pool
-        const rewardPaidFilter = orthogonalRewards.filters.RewardPaid(user.Address)
-        const rewardPaidEvents = await orthogonalRewards.queryFilter(rewardPaidFilter)
+            // Get all stake events and amounts to use for theoretical MPL amount calculation
+            const stakeFilter = rewardsContract.filters.Staked(user.address);
+            const stakeEvents = await rewardsContract.queryFilter(stakeFilter);
+            const totalTheoreticalRewards = await stakeEvents.reduce(async (total, obj, i, []) => {
+                const { timestamp: stakeTimestamp } = await provider.getBlock(obj.blockNumber);
+                return (await total).add(getTheoreticalRewards(obj.args.amount, currentTimestamp - stakeTimestamp));
+            }, BigNumber.from('0'));
 
-        // Get all MPL transferred from the governor address to account for past airdrops
-        const transferFilter = mpl.filters.Transfer(GOVERNOR, user.Address)
-        const transferEvents = await mpl.queryFilter(transferFilter)
+            const rewardsOwed = totalTheoreticalRewards.sub(totalClaimed).sub(totalAirdropped).sub(earnings);
 
-        // Get unclaimed MPL rewards for the user for the given pool
-        const earnings = (await orthogonalRewards.functions.earned(user.Address))[0]
+            return {
+                name: user.name,
+                address: user.address,
+                earnings: earnings.toString(),
+                totalClaimed: totalClaimed.toString(),
+                totalAirdropped: totalAirdropped.toString(),
+                totalTheoreticalRewards: totalTheoreticalRewards.toString(),
+                rewardsOwed: rewardsOwed.toString(),
+                rewardsOwedNormalized: parseInt(rewardsOwed.toString()) / 1e18,
+            };
+        })
+    );
 
-        // Parse and sum the total claimed MPL from events
-        const totalClaimed = rewardPaidEvents.reduce((total, obj, i, []) => {
-            return total.add(obj.args.reward)
-        }, BigNumber.from('0'))
+    // Save to file
+    const csv = new ObjectsToCsv(outputData);
+    await csv.toDisk(path.resolve(__dirname, 'csv', outputFilename));
+};
 
-        // Parse and sum the total transferred MPL from events
-        const totalAirdropped = transferEvents.reduce((total, obj, i, []) => {
-            return total.add(obj.args.value)
-        }, BigNumber.from('0'))
-
-        const totalStaked = (await orthogonalRewards.functions.balanceOf(user.Address))[0]
-
-        const timePassed = blockTimestamp - REWARD_ADDED_TIME_1;  // TODO: Should timestamp be an input?
-
-        // TODO: Add comment
-        const theoreticalRewards = 
-            BigNumber.from('2')
-                .mul(totalStaked)
-                .mul(BigNumber.from(timePassed.toString()))
-                .div(BigNumber.from('100'))
-                .div(BigNumber.from((180 * 86400).toString()))
-
-        const rewardsOwed = theoreticalRewards.sub(totalClaimed).sub(totalAirdropped).sub(earnings)
-
-        return {
-            name: user.Name,
-            address: user.Address,
-            earnings: earnings.toString(),
-            totalClaimed: totalClaimed.toString(),
-            totalAirdropped: totalAirdropped.toString(),
-            theoreticalRewards: theoreticalRewards.toString(),
-            rewardsOwed: rewardsOwed.toString()
-        }
-    }))
-
-    console.dir(outputData, {depth: null})
-}
-
-main();
-
-
+calcAirdrop('orthogonal-airdrop-input.csv', 'orthogonal-airdrop-output.csv', ORTHOGONAL_REWARDS, GOVERNOR, 1625625208);
